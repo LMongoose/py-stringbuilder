91d937f507afa03ba40f0f38b8b2c7f04c60850e
LMongoose <lukaspellezario@hotmail.com>
2020-11-26 22:03:41 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'stringbuilder',
+  packages = ['stringbuilder'],
+  version = '0.0.1',
+  license='GNU GPLv3',
+  description = 'TYPE YOUR DESCRIPTION HERE',
+  author = 'LMongoose',
+  author_email = 'lukaspellezario@hotmail.com',
+  url = 'https://github.com/LMongoose/py-stringbuilder',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['string', 'building', 'concatenation'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLV3)',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
