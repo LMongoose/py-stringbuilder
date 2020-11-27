2d3c6a26a77f9582998792097459ec059d54b263
LMongoose <lukaspellezario@hotmail.com>
2020-11-26 23:53:18 -0300

    setup update

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-  name = 'stringbuilder',
-  packages = ['stringbuilder'],
+  name = 'py-stringbuilder',
+  packages = ['py-stringbuilder'],
   version = '0.0.1',
-  license='GNU GPLv3',
+  license = 'GNU GPLv3',
   description = 'TYPE YOUR DESCRIPTION HERE',
+  long_description = long_description,
   author = 'LMongoose',
   author_email = 'lukaspellezario@hotmail.com',
   url = 'https://github.com/LMongoose/py-stringbuilder',
   download_url = 'https://github.com/LMongoose/py-stringbuilder/0.0.1.tar.gz',
   keywords = ['string', 'building', 'concatenation'],
-  install_requires=[],
-  classifiers=[
+  install_requires = [],
+  classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLV3)',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3.6',
   ],
+  python_requires='>=3.6'
 )
